96c4494260ea312bee530a0fc846c3fa4fd1ec58
erakshaykumar <akaakshay304@gmail.com>
2022-02-03 18:59:54 +0530

    added: addition of two numbers

---
+++ src/com/bridgelabz/javaproject/Addition.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.javaproject;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int sum = a+b;
+		System.out.println("Addition is: " +sum);
+
+	}
+
+}
